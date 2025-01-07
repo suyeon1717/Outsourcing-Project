2bb3770a850fad93eabf618f0f14f044e819df37
backswan0 <rnrwprh3434@gmail.com>
2025-01-07 12:26:18 +0900

    initial commit
    - test commit and push

--- src/main/java/com/example/outsourcingproject/OutsourcingProjectApplication.java
+++ src/main/java/com/example/outsourcingproject/OutsourcingProjectApplication.java
@@ -3,6 +3,7 @@ package com.example.outsourcingproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test
 @SpringBootApplication
 public class OutsourcingProjectApplication {
 
