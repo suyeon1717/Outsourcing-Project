e5d2b74cc4dbf26f3c2ce9db544f60b270f269f6
sy <tndus1886@naver.com>
2025-01-08 19:11:58 +0900

    feat: 회원 권한 ENUM 추가

---
+++ src/main/java/com/example/outsourcingproject/common/Authority.java
@@ -0,0 +1,10 @@
+package com.example.outsourcingproject.common;
+
+public enum Authority {
+    CUSTOMER("CUSTOMER"),
+    OWNER("OWNER");
+
+    private final String value;
+
+    Authority(String value) {this.value = value;}
+}
