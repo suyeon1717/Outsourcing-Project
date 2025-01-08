1e16a3254f1d114156761f76f02923264b65610f
sy <tndus1886@naver.com>
2025-01-08 19:12:23 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/example/outsourcingproject/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.outsourcingproject.common;
+
+import jakarta.persistence.Column;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    // 작성일, 수정일 필드는 Jpa Auditing 활용
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+}
